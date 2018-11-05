9ed066ff0f356d7ec67e2604d3f98aeab50df26d
Ben Furber <ben@fu.rber.io>
2018-11-05 15:25:24 +0000

    ✨ Add db:seed script for goals data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require "./db/data"
+
+local_goals = SeedData::GOALS
+
+local_goals.each do |goal|
+  goal_in_db = Goal.new()
+  goal_in_db.id = goal[:id]
+  goal_in_db.actions = goal[:actions]
+  goal_in_db.description = goal[:description]
+  goal_in_db.evidence = goal[:evidence]
+  goal_in_db.score = goal[:score]
+  goal_in_db.tag = goal[:tag]
+  goal_in_db.title = goal[:title]
+
+  goal_in_db.save
+end
+
+puts "Goals added"
