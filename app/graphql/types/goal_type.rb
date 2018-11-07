fda695cee11fabdff28e3c085220b87ac187d897
Ben Furber <ben@fu.rber.io>
2018-11-07 11:27:51 +0000

    🐛 Fix evidence field for goal type
    Was a string, now an array.

--- app/graphql/types/goal_type.rb
+++ app/graphql/types/goal_type.rb
@@ -5,7 +5,7 @@ module Types
     field :id, ID, null: false
     field :actions, [String], null: true
     field :description, String, null: true
-    field :evidence, String, null: true
+    field :evidence, [String], null: true
     field :score, Float, null: false
     field :tag, String, null: true
     field :title, String, null: false
