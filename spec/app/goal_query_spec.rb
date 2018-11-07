29449d5fba8dbee12b35310048e58a0d5c8fe08c
Ben Furber <ben@fu.rber.io>
2018-11-07 11:31:47 +0000

    ✅ Add test for full Goals graph query

---
+++ spec/app/goal_query_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+describe 'Goals query', type: :graphql do
+  it 'can execute a goals query' do
+    goal = create(:goal)
+    query = <<~QUERY
+      {
+        goals {
+          id
+          actions
+          description
+          evidence
+          score
+          tag
+          title
+        }
+      }
+    QUERY
+
+    result = execute(query)
+
+    expected = {
+      goals: [
+        {
+          actions: goal.actions,
+          description: goal.description,
+          evidence: goal.evidence,
+          id: goal.id.to_s,
+          score: goal.score,
+          tag: goal.tag,
+          title: goal.title
+        }
+      ]
+    }
+
+    expect(result[:data]).to eq(expected)
+  end
+end
