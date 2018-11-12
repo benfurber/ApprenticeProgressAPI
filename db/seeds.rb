a7ec392b7bc7d8bfefb5deeaa64766a46711991c
Ben Furber <ben@fu.rber.io>
2018-11-12 12:10:06 +0000

    ♻️ Refactor seeding to create user for goals

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,17 @@
 require "./db/data"
 
+def create_a_user
+  new_user = User.new
+  new_user.email = ENV["SEED_USER_EMAIL"]
+  new_user.password = ENV["SEED_USER_PASSWORD"]
+  new_user.save!
+  User.last
+end
+
+user = create_a_user
+
+puts "User added"
+
 local_goals = SeedData::GOALS
 
 local_goals.each do |goal|
@@ -11,6 +23,8 @@ local_goals.each do |goal|
   goal_in_db.score = goal[:score]
   goal_in_db.tag = goal[:tag]
   goal_in_db.title = goal[:title]
+
+  goal_in_db.user = user
 
   goal_in_db.save!
 end
